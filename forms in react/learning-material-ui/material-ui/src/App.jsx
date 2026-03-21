bdf045dd2386708d75f5a96c1accad7c19d014d3
Shivam <shivam.ug23@nsut.ac.in>
2026-03-21 18:52:11 +0530

    material-ui

---
+++ forms in react/learning-material-ui/material-ui/src/App.jsx
@@ -0,0 +1,15 @@
+ //import { useState } from 'react'
+import Button from '@mui/material/Button';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <button  variant="contained" > HELLO MATERIAL-UI</button>
+    </>
+  )
+}
+
+export default App
