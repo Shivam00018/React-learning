a386e03848d0aa86fa0b11de31ea2f51a4227ea7
Shivam <shivam.ug23@nsut.ac.in>
2026-03-22 14:47:28 +0530

    add operation functions

--- login signup project/Login-signup/src/Component/LoginSignup.jsx
+++ login signup project/Login-signup/src/Component/LoginSignup.jsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import './LoginSignup.css'
+
 import user_icon from '../Assets/person.png';
 import email_icon from '../Assets/email.png';
 import password_icon from '../Assets/password.png';
+
 export default function LoginSignup() {
+
+    let [Action, setAction] = useState("Login"); 
+
     return(
         <div className="container">
             <div className="header">
-                <div className="text">Sign-up</div>
+                <div className="text">{Action}</div>
                 <div className="underline"></div>
             </div>
 
             <div className="inputs">
-                <div className="input" > 
+                {Action === "Login"? <div></div>: <div className="input" > 
                     <img src={user_icon} alt=""  />
                     <input type="text" placeholder="Name" />
                     
-                </div>
+                </div>  }
+               
 
                 <div className="input" > 
                     <img src={email_icon} alt=""  />
@@ -29,10 +35,12 @@ export default function LoginSignup() {
                 </div>
             </div>
 
-            <div className="forgot-password">forget password? <span>Click here</span></div>
+            {Action === "Sign-up"?<div></div> :  <div className="forgot-password">forget password? <span>Click here</span></div> }
+     
+           
             <div className="submit-container">
-                <div className="sigup">Signup</div>
-                <div className="login">Login</div>
+                <div className={Action === "Sign-up"?"submit grey":"submit" } onClick={() => {setAction("Sign-up")}} >Signup</div>
+                <div className={Action === "Login"?"submit grey":"submit" } onClick={() => {setAction("Login")}}>Login</div>
             </div>
 
         </div>
